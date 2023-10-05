91e33b49a3845f0543f273282ed56f3d5e746248
estivenlopez05 <estivenlopez0511@gmail.com>
2023-10-05 14:45:46 -0500

    creando atributos, metodos construtor y setters setters

--- src/Empleados.java
+++ src/Empleados.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class Empleados {
+public class Empleados {
+    String nombreEmpleado;
+    String rol;
+    String fechaContratacion;
+
+    public Empleados(String nombreEmpleado, String rol, String fechaContratacion) {
+        this.nombreEmpleado = nombreEmpleado;
+        this.rol = rol;
+        this.fechaContratacion = fechaContratacion;
+
+    }
+
+    public String getNombreEmpleado() {
+        return nombreEmpleado;
+    }
+
+    public void setNombreEmpleado(String nombreEmpleado) {
+        this.nombreEmpleado = nombreEmpleado;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public String getFechaContratacion() {
+        return fechaContratacion;
+    }
+
+    public void setFechaContratacion(String fechaContratacion) {
+        this.fechaContratacion = fechaContratacion;
+    }
+
+    public void mostrarEmpleado() {
+        System.out.println("Nombre del empleado: " + nombreEmpleado);
+        System.out.println("Rol del empleado: " + rol);
+        System.out.println("Fecha de contratación: " + fechaContratacion);
+    }
+
 }
